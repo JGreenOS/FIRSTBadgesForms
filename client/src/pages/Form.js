5b08e4a4c28199b0aed10ce2d24196a6648580d1
Matthew Duffield <duffieldml@gmail.com>
2021-05-07 16:25:48 -0400

    Leadership 1&2 checklist rendering on the award form page currently

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -2,13 +2,20 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar.jsx'
 import { Container } from "../components/Grid"
+import LeadershipOne from '../components/Checklist/LeadershipOne.jsx'
+import LeadershipTwo from '../components/Checklist/LeadershipTwo.jsx'
 
 function Form () {
     return (
         <div>
             <Navbar />
             <Container fluid>
-                <p>form for assigning</p>
+            <label for="exampleFormControlTextarea2"><strong>Please enter the email address of the student you wish to award the badge to.</strong></label>
+            <input type="email" className="form-control" id="exampleFormControlInput1"
+            placeholder="name@example.com"/>
+                <LeadershipOne />
+                <br />
+                <LeadershipTwo />
             </Container>
         </div>
     )
