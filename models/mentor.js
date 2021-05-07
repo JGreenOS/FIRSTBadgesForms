ed7f9203ececac3eb0926abbb1ad4aabfe5a9945
jlw429 <jlw00329@gmail.com>
2021-05-06 22:50:14 -0400

    added models and some queries to api routes

---
+++ models/mentor.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+  const Mentor = sequelize.define(
+    'Mentor',
+    {
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 30],
+        },
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 30],
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 30],
+        },
+      },
+      team: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 30],
+        },
+      },
+    },
+    {
+      freezeTableName: true,
+      timestamps: false,
+    }
+  );
+  return Mentor;
+};
